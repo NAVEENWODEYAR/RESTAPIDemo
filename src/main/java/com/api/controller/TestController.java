cea8b73a7d964bd515ac089b0179912cd6cc8c59
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-10 23:41:25 +0530

    test EndPoint added for testing

---
+++ src/main/java/com/api/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class TestController {
+
+	@GetMapping("/test/{st}")
+	public String greetMsg(@PathVariable String st) {
+		return "Welcome to the family "+st;
+	}
+}
