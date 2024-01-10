c5f88e40654e95a8f6551f1e6244525ddd487a30
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-10 23:55:59 +0530

    logOut

--- src/main/java/com/api/controller/TestController.java
+++ src/main/java/com/api/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.api.controller;
 
+import java.util.Scanner;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
