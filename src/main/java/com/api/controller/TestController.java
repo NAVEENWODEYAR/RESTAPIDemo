430529ca0a4eb8d9dd766daa975e4bc67d286999
Naveen <naveenkrishnawodeyar@gmail.com>
2024-01-11 09:13:37 +0530

    @org.springframework.beans.factory.annotation.Autowired(required=true)

--- src/main/java/com/api/controller/TestController.java
+++ src/main/java/com/api/controller/TestController.java
@@ -1,21 +1,27 @@
 package com.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.api.service.TestService;
+
 @RestController
 @RequestMapping("/rest")
 public class TestController {
 
+	@Autowired
+	private TestService testService;
+	
 	@GetMapping("/test/{st}")
 	public String greetMsg(@PathVariable String st) {
 		return "Welcome to the family "+st;
 	}
 	
 	@GetMapping("/sum")
-	public int sum() {
+	public int add() {
 		return testService.add();
 	}
 }
--- src/main/java/com/api/service/TestService.java
+++ src/main/java/com/api/service/TestService.java
@@ -1,5 +1,15 @@
 package com.api.service;
 
-public class TestService {
+import java.util.Scanner;
 
+public class TestService {
+	
+	public int add() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("\nEnter the int values:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println("Sum "+(a+b));
+		return a+b;
+	}
 }
