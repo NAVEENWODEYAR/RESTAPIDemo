9a7a6b41313a930e35d2363e0ac645c26a38b221
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-27 09:21:45 +0530

    version 1.0 will be relesed

--- src/main/java/com/api/exception/MyCustomException.java
+++
@@ -1,8 +0,0 @@
-package com.api.exception;
-
-public class MyCustomException extends RuntimeException{
-
-	public MyCustomException(String st) {
-		super(st);
-	}
-}
--- src/main/java/com/api/repo/MyRepo.java
+++
@@ -1,8 +0,0 @@
-package com.api.repo;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MyRepo {
-
-}
