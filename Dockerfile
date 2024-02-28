c4a6cfea4b343997b0852f8628202997ccaa27d1
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-28 21:07:18 +0530

    entity defined

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-# Dockerfile, an blueprint to create docker images,
+#Dockerfile, an blueprint to create docker images,
 FROM openjdk:21
 RUN mkdir /usr/app/
 COPY target/RestAPIDemo.jar/ usr/app
 WORKDIR /usr/app/  
-ENTRYPOINT [ "java","-jar","RestAPIDemo.jar" ]
+ENTRYPOINT ["java","-jar","RestAPIDemo.jar"]
--- src/main/java/com/api/entity/Student.java
+++ src/main/java/com/api/entity/Student.java
@@ -1,5 +1,15 @@
 package com.api.entity;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    
+    private int stId;
+    private String stName;
+    private String stMail;
 }
