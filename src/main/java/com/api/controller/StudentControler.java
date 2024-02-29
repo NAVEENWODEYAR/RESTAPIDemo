94304dc0f2d6e472673fa15b1ac6d6319706e1ee
Naveen <naveenkrishnawodeyar@gmail.com>
2024-02-29 08:58:44 +0530

    test endpoint added

--- src/main/java/com/api/controller/StudentControler.java
+++ src/main/java/com/api/controller/StudentControler.java
@@ -1,8 +1,16 @@
 package com.api.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentControler {
-
+    
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public String greetmst() {
+	return "Welcome to the application,";
+    }
 }
