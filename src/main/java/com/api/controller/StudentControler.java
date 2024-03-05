f9681832dd53dc201c21e043a128fd6f83b5f9a6
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-05 09:08:16 +0530

    import org.springframework.web.bind.annotation

--- src/main/java/com/api/controller/StudentControler.java
+++ src/main/java/com/api/controller/StudentControler.java
@@ -2,10 +2,7 @@ package com.api.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rest")
@@ -19,6 +16,6 @@ public class StudentControler {
     
     @GetMapping("responseEntity")
     public ResponseEntity<String> response(){
-	return new ResponseEntity<String>("RESPONSEENTITY", HttpStatus.OK);
+	return new ResponseEntity<String>("RESPONSEENTITY", HttpStatus.ACCEPTED);
     }
 }
