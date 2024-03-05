63fd1ea5a9716c11e848c721cdda40f359595a8b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-05 09:05:34 +0530

    ResponseEntity<String> response()

--- src/main/java/com/api/controller/StudentControler.java
+++ src/main/java/com/api/controller/StudentControler.java
@@ -1,6 +1,7 @@
 package com.api.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,9 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class StudentControler {
     
-    @GetMapping
+    @GetMapping("/msg")
     @ResponseStatus(code = HttpStatus.OK)
     public String greetMsg() {
 	return "Welcome to the application,";
+    }
+    
+    @GetMapping("responseEntity")
+    public ResponseEntity<String> response(){
+	return new ResponseEntity<String>("RESPONSEENTITY", HttpStatus.OK);
     }
 }
