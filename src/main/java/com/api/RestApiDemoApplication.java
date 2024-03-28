714063179ae86c3d4a5113bee49e52feafb300b0
Naveen <naveenkrishnawodeyar@gmail.com>
2024-03-28 22:24:24 +0530

    Thread.currentThread

--- src/main/java/com/api/RestApiDemoApplication.java
+++ src/main/java/com/api/RestApiDemoApplication.java
@@ -8,5 +8,6 @@ public class RestApiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiDemoApplication.class, args);
+		System.out.println(Thread.currentThread().getName());
 	}
 }
