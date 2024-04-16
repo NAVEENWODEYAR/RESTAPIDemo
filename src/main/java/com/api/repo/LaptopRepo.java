fab1ef27eeb01185d92edda5fa860879ed5124b9
Naveen <naveenkrishnawodeyar@gmail.com>
2024-04-16 23:43:42 +0530

    logOut

--- src/main/java/com/api/repo/LaptopRepo.java
+++ src/main/java/com/api/repo/LaptopRepo.java
@@ -3,9 +3,11 @@ package com.api.repo;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.api.entity.Laptop;
 
+@Repository
 public interface LaptopRepo extends JpaRepository<Laptop, UUID> {
 
 }
