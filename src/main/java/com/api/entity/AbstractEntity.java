14f61d9768210ebbb667789d6ac9cd5d970cc614
Naveen <naveenkrishnawodeyar@gmail.com>
2024-04-21 00:19:45 +0530

    dates added in entity

--- src/main/java/com/api/entity/AbstractEntity.java
+++ src/main/java/com/api/entity/AbstractEntity.java
@@ -1,10 +1,30 @@
 package com.api.entity;
 
 import java.sql.Date;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 public class AbstractEntity {
     
-    private Date createdDate;
-    private Date modifiedDate;
-
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+    
+    @Column(name = "CREATED_DATE")
+    private LocalDateTime createdDate;
+    
+    @Column(name = "MODIFIED_BY")
+    private String modifiedBy;
+    
+    @Column(name = "MODIFIED_DATE")
+    private LocalDateTime modifiedDate;
+    
+    @PrePersist
+    protected void created() {createdDate = LocalDateTime.now();}
+    
+    @PreUpdate
+    protected void updated() { modifiedDate = LocalDateTime.now();}
+    
 }
