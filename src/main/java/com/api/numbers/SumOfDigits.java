4e19444f13f2eb7073177296effbd87b70716a4b
Naveen <naveenkrishnawodeyar@gmail.com>
2024-05-25 14:52:09 +0530

    sumOfdigits in java8

---
+++ src/main/java/com/api/numbers/SumOfDigits.java
@@ -0,0 +1,14 @@
+
+package com.api.numbers;
+
+/**
+ * @autor Navee K Wodeyar
+ * @date 25/05/2024
+ * Find sum of all digits of a number in Java8
+ */
+public class SumOfDigits {
+
+	public SumOfDigits() {
+	}
+
+}
