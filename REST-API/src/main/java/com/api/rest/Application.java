44a76393ce27ab3aa1f3ee164af1f1fb5ae3cbef
Naveen <naveenkrishnawodeyar@gmail.com>
2024-09-15 17:46:59 +0530

    Java Version

--- REST-API/src/main/java/com/api/rest/Application.java
+++ REST-API/src/main/java/com/api/rest/Application.java
@@ -1,7 +1,9 @@
 package com.api.rest;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +12,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 		System.out.println(Thread.currentThread().getName()+"-"+Thread.currentThread().getPriority());
 	}
-
+    @Bean
+    public CommandLineRunner printJavaVersion() {
+        return args -> {
+            String javaVersion = System.getProperty("java.version");
+            System.out.println("\n#############################\n");
+            System.out.println("JAVA VERSiON: " + javaVersion);
+            System.out.println("\n#############################\n");
+        };
+    }
 }
